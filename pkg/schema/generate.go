ebd3534ed6aac863517fb8c3617a587d90b433ba
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-08-18 19:34:20 -0500

    Idea formulating

--- pkg/schema/generate.go
+++ pkg/schema/generate.go
@@ -1,15 +1,33 @@
 package schema
 
 import (
-	"go/types"
+	"go/ast"
+	"go/token"
 
 	"github.com/pb33f/libopenapi/datamodel/high/base"
 )
 
-func Struct(schema *base.Schema) *types.Struct {
-	return types.NewStruct(Fields(schema), nil)
+func Struct(schema *base.Schema) ast.Decl {
+	return &ast.GenDecl{
+		Doc: &ast.CommentGroup{},
+		Tok: token.TYPE,
+		Specs: []ast.Spec{&ast.TypeSpec{
+			Name: ast.NewIdent(schema.Title),
+			Type: &ast.StructType{
+				Fields: Fields(schema),
+			},
+		}},
+	}
 }
 
-func Fields(schema *base.Schema) []*types.Var {
-	return []*types.Var{}
+func Fields(schema *base.Schema) *ast.FieldList {
+	list := &ast.FieldList{}
+	for name := range schema.Properties.FromOldest() {
+		list.List = append(list.List, &ast.Field{
+			Names: []*ast.Ident{ast.NewIdent(name)},
+			Type:  ast.NewIdent("string"),
+		})
+	}
+
+	return list
 }
